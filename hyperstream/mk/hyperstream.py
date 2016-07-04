884cd50cbf91520c2c2794a7f0c30a8467e44713
Meelis Kull <meelis.kull@gmail.com>
2016-07-04 10:51:15 +0100

    mk: hyperstream.py first working prototype

--- hyperstream/mk/hyperstream.py
+++ hyperstream/mk/hyperstream.py
@@ -38,6 +38,8 @@ class TimeIntervals: # example object: (t1,t2]U(t3,t4]U...
     self.value=value
   def __str__(self):
     return("U".join(["("+str(a)+","+str(b)+"]" for (a,b) in self.value]))
+  def __repr__(self):
+    return(str(self))
   def split(self,points):
     if len(points)==0:
       return
@@ -81,22 +83,22 @@ class TimeIntervals: # example object: (t1,t2]U(t3,t4]U...
     new.compress()
     return(new)
 
-class StreamTool: 
+class Tool: 
   # inputs a mixed list of parameters and StreamRef and lists of StreamRef
   # outputs a stream
   def __init__(self):
     pass
-  def run(self,start,end,**kwargs):
+  def calc(self,start,end,**kwargs):
     raise NotImplementedError
 
-class DocumentTool:
+class TimedTool(Tool):
   # inputs a mixed list of parameters and StreamRef and lists of StreamRef
   # outputs a single document at a time, the times are specified by a timing stream
   def __init__(self):
     pass
-  def run_single(self,timestamp,**kwargs):
+  def calc_document(self,timestamp,**kwargs):
     raise NotImplementedError
-  def run(self,start,end,timer=None,**kwargs):
+  def calc(self,start,end,timer=None,**kwargs):
     if timer.__class__!=StreamRef:
       raise RuntimeError
     documents = []
@@ -106,24 +108,24 @@ class DocumentTool:
       for kw in kwargs:
 	arg = kwargs[kw]
 	if arg.__class__==StreamRef:
-	  kwargs2[kw] = arg.get_data(timestamp-epsilon,timestamp)
+	  kwargs2[kw] = arg.get_documents(timestamp-epsilon,timestamp)
 	elif arg.__class__==list:
 	  arg_list = []
 	  for arg_i in arg:
 	    if arg_i.__class__==StreamRef:
-	      arg_list.append(arg_i.get_data(timestamp-epsilon,timestamp))
+	      arg_list.append(arg_i.get_documents(timestamp-epsilon,timestamp))
 	    else:
 	      arg_list.append(arg_i)
 	  kwargs2[kw] = arg_list
 	else:
 	  kwargs2[kw] = arg
-      documents.extend(self.run_single(timestamp,kwargs))
+      documents.append(Document(timestamp,self.calc_document(timestamp,**kwargs2)))
     return(documents)
     
 
-class Clock(StreamTool):
-#  def run(self,start,end,**kwargs):
-  def run(self,start,end,first=date(1970,1,1),stride=delta(seconds=1)):
+class Clock(Tool):
+#  def calc(self,start,end,**kwargs):
+  def calc(self,start,end,first=date(1970,1,1),stride=delta(seconds=1)):
 #    first = kwargs['first']
 #    stride = kwargs['stride']
     n_strides = int( (start-first).total_seconds() // stride.total_seconds() )
@@ -137,16 +139,41 @@ class Clock(StreamTool):
 #    time_stream(stride=delta(seconds=1),first=date(1970,1,1)):
 
 class Constant(Tool):
-  def run(self,start,end,times=None,value=None):
+  def calc(self,start,end,times=None,value=None):
     documents = []
-    for tdoc in times:
+    for tdoc in times.get_documents(start,end):
       documents.append(Document(tdoc.timestamp,value))
     return(documents)
+
+class Constant2(TimedTool):
+  def calc_document(self,timestamp,value=None):
+    return(value)
+
+class Pool(TimedTool):
+  def calc_document(self,timestamp,data=None,document_func=None,data_func=None):
+    print(timestamp)
+    print(data)
+    if document_func!=None:
+      assert data_func==None
+      return(document_func(data))
+    if data_func!=None:
+      assert document_func==None
+      return(data_func([doc.data for doc in data]))
+    assert False
+#     - returns a stream with documents at exactly the same times as the 'times' stream
+#     - each document is calculated by pooling together the documents extracted from the 'data' stream according to the use_stream parameters
+#     - 'data' and 'times' must be streams represented as records returned by the use_stream function
+#     - pooling is done by calling the function corresponding to the value of 'operator'
+#     - allowed values are 'min', 'max', 'sum', 'mean', 'median','integrate_per_sec',...
 
 class Document:
   def __init__(self,timestamp,data):
     self.timestamp = timestamp
     self.data = data
+  def __str__(self):
+    return('TIME: '+str(self.timestamp)+' DATA: '+str(self.data))
+  def __repr__(self):
+    return(str(self))
 
 class Stream:
   def __init__(self,tool,temporary,**kwargs):
@@ -161,22 +188,24 @@ class Stream:
       kwargs = {}
       for kw in self.kwargs:
 	arg = self.kwargs[kw]
-	if arg.__class__==StreamRef:
-	  kwargs[kw] = arg.get_data(s,e)
-	elif arg.__class__==list:
-	  arg_list = []
-	  for arg_i in arg:
-	    if arg_i.__class__==StreamRef:
-	      arg_list.append(arg_i.get_data(s,e))
-	    else:
-	      arg_list.append(arg_i)
-	  kwargs[kw] = arg_list
-	else:
-	  kwargs[kw] = arg
+	kwargs[kw] = arg # temporary
+#	if arg.__class__==StreamRef:
+#	  kwargs[kw] = arg.get_data(s,e)
+#	elif arg.__class__==list:
+#	  arg_list = []
+#	  for arg_i in arg:
+#	    if arg_i.__class__==StreamRef:
+#	      arg_list.append(arg_i.get_data(s,e))
+#	    else:
+#	      arg_list.append(arg_i)
+#	  kwargs[kw] = arg_list
+#	else:
+#	  kwargs[kw] = arg
       print('Running the tool '+self.tool.__class__.__name__+' from '+str(s)+' to '+str(e))
-      documents = self.tool.run(s,e,**kwargs)
+      documents = self.tool.calc(s,e,**kwargs)
       self.documents.extend(documents)
     self.times = self.times+TimeIntervals([(start,end)])
+    return
   def get_documents(self,start,end):
     self.calc(start,end)
     documents = [doc for doc in self.documents if (doc.timestamp>start) and (doc.timestamp<=end)]
@@ -187,21 +216,20 @@ class Stream:
     
     
 class StreamRef:
-  def __init__(self,stream,start=date(1970,1,1),end=delta(0),sfilter=None,tail=None,use_only_data=False):
+  def __init__(self,stream,start=date(1970,1,1),end=delta(0),sfilter=None,tail=None):
     self.stream = stream
     self.start = start
     self.end = end
     self.tail = tail
     self.sfilter = sfilter
-    self.use_only_data = use_only_data
-  def get_data(self,start,end):
+  def __get_doc_and_data(self,start,end,only_data=False):
     s = self.start
     if s.__class__==delta:
       s = start+s
     e = self.end
     if e.__class__==delta:
       e = end+e
-    if self.use_only_data:
+    if only_data:
       data = self.stream.get_data(s,e)      
     else:
       data = self.stream.get_documents(s,e)      
@@ -210,6 +238,10 @@ class StreamRef:
       return(data)
     else:
       return(data[-self.tail:])
+  def get_documents(self,start,end):
+    return(self.__get_doc_and_data(start,end,only_data=False))
+  def get_data(self,start,end):
+    return(self.__get_doc_and_data(start,end,only_data=True))
     
 class StreamBase:
   def __init__(self):
@@ -223,71 +255,88 @@ d2 = s.get_data(date(2000,3,1,12,29),date(2000,3,1,13,15))
 s = Stream(Clock(),False,stride=delta(minutes=2))
 d = s.get_data(date(2000,3,1,12),date(2000,3,1,13))
 d2 = s.get_data(date(2000,3,1,12,29),date(2000,3,1,13,15))
-s2 = Stream(Constant(),False,times=StreamRef(s,-epsilon,delta(0)),value=42)
+s2 = Stream(Constant(),False,times=StreamRef(s,delta(0)),value=42)
 d3 = s2.get_data(date(2000,3,1,12,29),date(2000,3,1,13,15))
 d4 = s2.get_documents(date(2000,3,1,12,29),date(2000,3,1,13,15))
+s3 = Stream(Constant2(),False,timer=StreamRef(s,delta(0)),value=42)
+d5 = s3.get_documents(date(2000,3,1,12,29),date(2000,3,1,13,15))
+d6 = s3.get_data(date(2000,3,1,12,29),date(2000,3,1,13,15))
+s4 = Stream(Clock(),False,stride=delta(minutes=5))
+s5 = Stream(Pool(),False,timer=StreamRef(s4,delta(0)),data=StreamRef(s3,-delta(minutes=5)),data_func=sum)
+d7 = s5.get_data(date(2000,3,1,12,20),date(2000,3,1,13,10))
+d8 = s5.get_documents(date(2000,3,1,12,20),date(2000,3,1,13,10))
 
-def def_stream(stream_func,**kwargs):
-  return(0)
-#     - this defines a new stream, which can be calculated with the 'stream_func'
-#     - the remaining parameters must be named and will be passed on to 'stream_func' during the calculation
-#     - if some existing stream has an identical definition, then an integer representing the identifier of that stream is returned
-#     - otherwise, a new stream is created and the identifier to that stream is returned
-#     - this function does not calculate the stream, it only defines the stream
+#  def calc_document(self,timestamp,data=None,document_func=None,data_func=None):
 
-def def_temporary_stream(stream_func,**kwargs):
-  return(0)
-#     - the same as def_stream, except that the data in the defined stream are never stored in the MongoDB
-#     - instead, the data are always recalculated (or kept in memory if this possibility is implemented)
 
-def use_stream(stream_id,start=date(1970,1,1),end=delta(0),tail=None,sfilter=None):
-  return(0)
-#     - this function should be called when defining new streams that depend on the existing stream with the identifier 'stream_id'
-#     - e.g., def_stream(stream_func,input_stream=use_stream(stream,...))
-#     - it returns a data structure defining which stream and which documents of that stream are going to be used
-#     - by default, the whole stream up to the current moment is going to be used
-#     - i.e., the whole stream up to now must be calculated in order to calculate the new stream
-#     - documents outside of the range from 'start' to 'end' are not going to be used
-#     - 'start' and 'end' can be defined either with 'date' (datetime.datetime) or 'delta' (datetime.timedelta)
-#    - if defined by 'delta' then times are taken relative to the time in the new stream that uses this stream
-#     - e.g., start=delta(seconds=-5) and end=delta(seconds=5) mean that 5 seconds from the past and 5 seconds from the future will be used
-#     - i.e., the new stream can be calculated only with a delay of 5 seconds
-#     - if 'sfilter' is not 'None' then it must define a stream filter allowing to use a subselection of stream (format to be determined, e.g. MongoDB query format)
-#     - if 'tail' is not 'None' then it must be a non-negative integer specifying the number of last documents used in the time interval from 'start' to 'end' after applying 'sfilter'
 
-#################################################
-### FUNCTIONS TO CREATE AND CALCULATE STREAMS ###
-#################################################
 
-# How to call these things?
-# worker (Taverna old)
-# service (Taverna new)
-# component (Kepler general)
-# actor (Kepler specific)
-# director (Kepler specific)
-# tool (Galaxy)
-# processor (?)
 
-def time_stream(stride=delta(seconds=1),first=date(1970,1,1)):
-  return(0)
-#     - returns a stream with documents at regular intervals
-#     - each document contains information about the current time (datetime.datetime)
-#     - the first document is at the datetime 'first'
-#     - every next one is after 'stride' timedelta
-#     - 'stride' must be positive, defined with 'delta' (i.e. datetime.timedelta)
-#     - allowed parameters to 'delta' are 'days','weeks','hours','minutes','seconds','milliseconds','microseconds'
-#     - the problems of leap seconds (23:59:60, see https://en.wikipedia.org/wiki/Leap_second) and daylight saving time are not dealt with, i.e. treated the same way as the datetime module does it
 
-def merge(stream_list):
-  return(0)
-#     - returns a stream that is merged together from the streams in 'stream_list'
-#     - the ordering is first by timestamp and second by the order within the 'stream_list'
 
-def pool(data,times,operator,default):
-  return(0)
-#     - returns a stream with documents at exactly the same times as the 'times' stream
-#     - each document is calculated by pooling together the documents extracted from the 'data' stream according to the use_stream parameters
-#     - 'data' and 'times' must be streams represented as records returned by the use_stream function
-#     - pooling is done by calling the function corresponding to the value of 'operator'
-#     - allowed values are 'min', 'max', 'sum', 'mean', 'median','integrate_per_sec',...
 
+### 
+### def def_stream(stream_func,**kwargs):
+###   return(0)
+### #     - this defines a new stream, which can be calculated with the 'stream_func'
+### #     - the remaining parameters must be named and will be passed on to 'stream_func' during the calculation
+### #     - if some existing stream has an identical definition, then an integer representing the identifier of that stream is returned
+### #     - otherwise, a new stream is created and the identifier to that stream is returned
+### #     - this function does not calculate the stream, it only defines the stream
+### 
+### def def_temporary_stream(stream_func,**kwargs):
+###   return(0)
+### #     - the same as def_stream, except that the data in the defined stream are never stored in the MongoDB
+### #     - instead, the data are always recalculated (or kept in memory if this possibility is implemented)
+### 
+### def use_stream(stream_id,start=date(1970,1,1),end=delta(0),tail=None,sfilter=None):
+###   return(0)
+### #     - this function should be called when defining new streams that depend on the existing stream with the identifier 'stream_id'
+### #     - e.g., def_stream(stream_func,input_stream=use_stream(stream,...))
+### #     - it returns a data structure defining which stream and which documents of that stream are going to be used
+### #     - by default, the whole stream up to the current moment is going to be used
+### #     - i.e., the whole stream up to now must be calculated in order to calculate the new stream
+### #     - documents outside of the range from 'start' to 'end' are not going to be used
+### #     - 'start' and 'end' can be defined either with 'date' (datetime.datetime) or 'delta' (datetime.timedelta)
+### #    - if defined by 'delta' then times are taken relative to the time in the new stream that uses this stream
+### #     - e.g., start=delta(seconds=-5) and end=delta(seconds=5) mean that 5 seconds from the past and 5 seconds from the future will be used
+### #     - i.e., the new stream can be calculated only with a delay of 5 seconds
+### #     - if 'sfilter' is not 'None' then it must define a stream filter allowing to use a subselection of stream (format to be determined, e.g. MongoDB query format)
+### #     - if 'tail' is not 'None' then it must be a non-negative integer specifying the number of last documents used in the time interval from 'start' to 'end' after applying 'sfilter'
+### 
+### #################################################
+### ### FUNCTIONS TO CREATE AND CALCULATE STREAMS ###
+### #################################################
+### 
+### # How to call these things?
+### # worker (Taverna old)
+### # service (Taverna new)
+### # component (Kepler general)
+### # actor (Kepler specific)
+### # director (Kepler specific)
+### # tool (Galaxy)
+### # processor (?)
+### 
+### def time_stream(stride=delta(seconds=1),first=date(1970,1,1)):
+###   return(0)
+### #     - returns a stream with documents at regular intervals
+### #     - each document contains information about the current time (datetime.datetime)
+### #     - the first document is at the datetime 'first'
+### #     - every next one is after 'stride' timedelta
+### #     - 'stride' must be positive, defined with 'delta' (i.e. datetime.timedelta)
+### #     - allowed parameters to 'delta' are 'days','weeks','hours','minutes','seconds','milliseconds','microseconds'
+### #     - the problems of leap seconds (23:59:60, see https://en.wikipedia.org/wiki/Leap_second) and daylight saving time are not dealt with, i.e. treated the same way as the datetime module does it
+### 
+### def merge(stream_list):
+###   return(0)
+### #     - returns a stream that is merged together from the streams in 'stream_list'
+### #     - the ordering is first by timestamp and second by the order within the 'stream_list'
+### 
+### def pool(data,times,operator,default):
+###   return(0)
+### #     - returns a stream with documents at exactly the same times as the 'times' stream
+### #     - each document is calculated by pooling together the documents extracted from the 'data' stream according to the use_stream parameters
+### #     - 'data' and 'times' must be streams represented as records returned by the use_stream function
+### #     - pooling is done by calling the function corresponding to the value of 'operator'
+### #     - allowed values are 'min', 'max', 'sum', 'mean', 'median','integrate_per_sec',...
+### 
