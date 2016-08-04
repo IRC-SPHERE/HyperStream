d7f9fea45376bbb8b59ff4eb4ba033039a2c8d7f
Tom Diethe <tdiethe@gmail.com>
2016-08-04 16:27:04 +0100

    Reorganisation for full development

---
+++ deprecated/hyperstream/models/stream_instance.py
@@ -0,0 +1,54 @@
+"""
+The MIT License (MIT)
+Copyright (c) 2014-2017 University of Bristol
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+from mongoengine import Document, DateTimeField, StringField, DictField, DynamicField
+from ..utils import Printable
+
+
+class StreamInstanceModel(Document):
+    stream_id = StringField(required=True, min_length=1, max_length=512)
+    stream_type = StringField(required=True, min_length=1, max_length=512)
+    datetime = DateTimeField(required=True)
+    filters = DictField(required=False)
+    metadata = DictField(required=False)
+    version = StringField(required=True, min_length=1, max_length=512)
+    value = DynamicField(required=True)
+
+    meta = {
+        'collection': 'streams',
+        'indexes': [{'fields': ['stream_id']}],
+        'ordering': ['start']
+    }
+
+
+class StreamInstance(Printable):
+    def __init__(self, stream_id, stream_type, datetime, filters, metadata, version, value):
+        self.stream_id = stream_id
+        self.stream_type = stream_type
+        self.datetime = datetime
+        self.filters = filters
+        self.metadata = metadata
+        self.version = version
+        self.value = value
+
+    def to_model(self):
+        return StreamInstanceModel(**self.__dict__)
