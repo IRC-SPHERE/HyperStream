3b29702e462fb4080724f6f6712df9bd8f2affbc
Tom Diethe <tdiethe@gmail.com>
2016-08-09 17:13:20 +0100

    Added modifiers package. Added some more bases.

---
+++ bases/file_base.py
@@ -0,0 +1,77 @@
+"""
+The MIT License (MIT)
+Copyright (c) 2014-2017 University of Bristol
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+from memory_base import ReadOnlyMemoryBase
+from ..modifiers import Identity
+from dateutil.parser import parse
+import os
+from datetime import datetime
+
+
+class FileBase(ReadOnlyMemoryBase):
+    """
+    An abstract stream base where the streams are recursive sub-folders under a given path and documents correspond to
+    all those files which have a timestamp as their prefix in the format yyyy_mm_dd_hh_mm_ss_mmm_*.
+    All the derived classes must override the function data_loader(short_path,file_long_name) which determines how the
+    data are loaded into the document of the stream.
+    The files of the described format must never be deleted.
+    The call update(up_to_timestamp) must not be called unless it is guaranteed that later no files with earlier
+    timestamps are added.
+    """
+
+    def get_stream_writer(self, stream_id):
+        raise NotImplementedError()
+
+    def create_stream(self, stream_def):
+        raise NotImplementedError()
+
+    def __init__(self, base_id, path, up_to_timestamp=MIN_DATE):
+        self.path = path
+        super(FileBase, self).__init__(base_id, up_to_timestamp)
+
+    def repr_stream(self, stream_id):
+        s = 'externally defined by the file system, read-only stream'
+        s = s + ', currently holding ' + str(len(self.streams[stream_id])) + ' files'
+        return s
+
+    def file_filter(self, sorted_file_names):
+        for file_long_name in sorted_file_names:
+            date_string, version = file_long_name.split('_')
+            yield (parse(date_string), version, file_long_name)
+
+    def update_streams(self, up_to_timestamp):
+        path = self.path
+        for (long_path, dir_names, file_names) in os.path.walk(path):
+            short_path = long_path[len(path) + 1:]
+            stream_id = short_path
+            stream = []
+            self.streams[stream_id] = stream
+            for (file_timestamp, file_short_name, file_long_name) in self.file_filter(sorted(file_names)):
+                if file_timestamp <= up_to_timestamp:
+                    file_timestamp_str = file_long_name[:23]
+                    stream.append((file_timestamp, self.data_loader(short_path, file_long_name)))
+
+    def data_loader(self, short_path, file_long_name):
+        raise NotImplementedError
+
+    def get_default_ref(self):
+        return {'start': datetime.min, 'end': self.up_to_timestamp, 'modifier': Identity()}
---
+++ modifiers/simple_modifiers.py
@@ -0,0 +1,229 @@
+"""
+The MIT License (MIT)
+Copyright (c) 2014-2017 University of Bristol
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+from modifier import Modifier
+
+
+class Identity(Modifier):
+    def __init__(self):
+        super(Identity, self).__init__()
+        self.types = {'data_gen': 'data_gen', 'doc_gen': 'doc_gen', 'data': 'data', 'doc': 'doc'}
+
+    def __call__(self, data):
+        return data
+
+
+class First(Modifier):
+    def __init__(self):
+        super(First, self).__init__()
+        self.types = {'data_gen': 'data', 'doc_gen': 'doc'}
+
+    def __call__(self, data):
+        for d in data:
+            return d
+        return None
+
+
+class Last(Modifier):
+    def __init__(self):
+        super(Last, self).__init__()
+        self.types = {'data_gen': 'data', 'doc_gen': 'doc'}
+
+    def __call__(self, data):
+        res = None
+        for d in data:
+            res = d
+        return res
+
+
+class Head(Modifier):
+    def __init__(self, n):
+        super(Head, self).__init__()
+        self.types = {'data_gen': 'data_gen', 'doc_gen': 'doc_gen'}
+        self.n = n
+
+    def __call__(self, data):
+        i = 0
+        for d in data:
+            i += 1
+            if i > self.n:
+                break
+            yield d
+
+
+class Tail(Modifier):
+    def __init__(self, n):
+        super(Tail, self).__init__()
+        self.types = {'data_gen': 'data_gen', 'doc_gen': 'doc_gen'}
+        self.n = n
+
+    def __call__(self, data):
+        tail = ()
+        for d in data:
+            tail = (tail + (d,))[-self.n:]
+        for d in tail:
+            yield d
+
+
+class Data(Modifier):
+    def __init__(self):
+        super(Data, self).__init__()
+        self.types = {'doc_gen': 'data_gen'}
+
+    def __call__(self, doc_gen):
+        for (time, data) in doc_gen:
+            yield data
+
+
+class Time(Modifier):
+    def __init__(self):
+        super(Time, self).__init__()
+        self.types = {'doc_gen': 'data_gen'}
+
+    def __call__(self, doc_gen):
+        for (time, data) in doc_gen:
+            yield time
+
+
+class IData(Modifier):
+    def __init__(self):
+        super(IData, self).__init__()
+        self.types = {'doc': 'data'}
+
+    def __call__(self, doc):
+        (time, data) = doc
+        return data
+
+
+class ITime(Modifier):
+    def __init__(self):
+        super(ITime, self).__init__()
+        self.types = {'doc': 'data'}
+
+    def __call__(self, doc):
+        (time, data) = doc
+        return time
+
+
+class List(Modifier):
+    def __init__(self):
+        super(List, self).__init__()
+        self.types = {'data_gen': 'data', 'doc_gen': 'data'}
+
+    def __call__(self, data):
+        return list(data)
+
+
+class Component(Modifier):
+    def __init__(self, key):
+        super(Component, self).__init__()
+        self.types = {'doc_gen': 'doc_gen'}
+        self.key = key
+
+    def __call__(self, doc_gen):
+        for (time, data) in doc_gen:
+            try:
+                yield (time, data[self.key])
+            except KeyError:
+                pass
+
+
+class CompFilter(Modifier):
+    def __init__(self, key, values):
+        super(CompFilter, self).__init__()
+        self.types = {'doc_gen': 'doc_gen'}
+        self.key = key
+        self.values = values
+
+    def __call__(self, doc_gen):
+        for (time, data) in doc_gen:
+            try:
+                if data[self.key] in self.values:
+                    yield (time, data)
+            except KeyError:
+                pass
+
+
+class DelNones(Modifier):
+    def __init__(self):
+        super(DelNones, self).__init__()
+        self.types = {'doc_gen': 'doc_gen'}
+
+    def __call__(self, doc_gen):
+        for (time, data) in doc_gen:
+            data2 = {}
+            for (key, value) in data.items():
+                if value is not None:
+                    data2[key] = value
+            yield (time, data2)
+
+
+class Sum(Modifier):
+    def __init__(self, first=0):
+        super(Sum, self).__init__()
+        self.types = {'data_gen': 'data'}
+        self.first = first
+
+    def __call__(self, data_gen):
+        res = self.first
+        for data in data_gen:
+            res = res + data
+        return res
+
+
+class Product(Modifier):
+    def __init__(self, first=1):
+        super(Product, self).__init__()
+        self.types = {'data_gen': 'data'}
+        self.first = first
+
+    def __call__(self, data_gen):
+        res = self.first
+        for data in data_gen:
+            res = res * data
+        return res
+
+
+class Average(Modifier):
+    def __init__(self):
+        super(Average, self).__init__()
+        self.types = {'data_gen': 'data'}
+
+    def __call__(self, data_gen):
+        res = 0
+        count = 0
+        for data in data_gen:
+            res = res + data
+            count += 1
+        return res / count
+
+
+class Count(Modifier):
+    def __init__(self):
+        super(Count, self).__init__()
+        self.types = {'data_gen': 'data'}
+
+    def __call__(self, data_gen):
+        count = 0
+        for _ in data_gen:
+            count += 1
+        return count
