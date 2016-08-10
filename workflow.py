bebb83b380d66854c3e5a2384148845fbc5aeeee
Tom Diethe <tdiethe@gmail.com>
2016-08-10 14:55:16 +0100

    Some workflow logic

--- workflow.py
+++ workflow.py
@@ -24,19 +24,53 @@ from utils import Printable
 from models import WorkflowDefinitionModel, PlateDefinitionModel
 
 
+class Node(Printable):
+    def __init__(self, node_id, stream_id, plates):
+        self.node_id = node_id
+        self.stream_id = stream_id
+        self.plate_ids = plates
+
+
+class Factor(Printable):
+    def __init__(self, plates, factor_definition):
+        # TODO: Check here that the tool exists
+        self.tool = factor_definition.tool
+
+        for plate in plates:
+            # TODO: Here we have to check the plate definitions
+            pass
+
+        self.sources = factor_definition.sources
+        self.sink = factor_definition.sink
+
+
 class Plate(Printable):
     def __init__(self, plate_definition):
         self.plate_definition = plate_definition
 
 
 class Workflow(Printable):
-    def __init__(self, workflow_definition):
-        self.workflow_definition = workflow_definition
+    def __init__(self, channels, plates, workflow_definition):
+        self.channels = channels
 
-    def execute(self, sphere_connector):
+        self.workflow_id = workflow_definition.workflow_id
+        self.name = workflow_definition.name
+        self.owner = workflow_definition.owner
+
+        self.nodes = {}
+        for node_id in workflow_definition.nodes:
+            node = workflow_definition.nodes[node_id]
+            # TODO: Check stream ID exists in stream_ids
+            p = [plates[plate_id] for plate_id in node.plate_ids]
+            self.nodes[node_id] = Node(node_id, node.stream_id, p)
+
+        self.factors = [Factor(plates, f) for f in workflow_definition.factors]
+
+        # TODO: Here we have to check that all of the components of the workflow exist in each of the channels
+
+    def execute(self):
         """
         Here we need to work out in which channels each of the streams in the workflow live
-        :param sphere_connector:
         :return:
         """
         pass
@@ -46,14 +80,17 @@ class WorkflowManager(Printable):
     workflows = {}
     plates = {}
 
-    def __init__(self):
+    def __init__(self, channels):
+        self.channels = channels
+
+        # Plate definitions (arrays of streams)
         for p in PlateDefinitionModel.objects:
             self.plates[p.plate_id] = Plate(p)
 
         # TODO: Make sure all of the plates in the workflow definitions exist in the plate definitions
         for f in WorkflowDefinitionModel.objects:
-            self.workflows[f.workflow_id] = Workflow(f)
+            self.workflows[f.workflow_id] = Workflow(channels, self.plates, f)
 
-    def execute_all(self, sphere_connector):
+    def execute_all(self):
         for workflow in self.workflows:
-            self.workflows[workflow].execute(sphere_connector)
+            self.workflows[workflow].execute()
