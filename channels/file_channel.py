46dd05725c386dede9ca764861296072d8456ce7
Niall Twomey <niall.twomey@bristol.ac.uk>
2016-08-15 13:59:41 +0100

    Added file date time class. About to rename all tools to be majorversion.minorversion.patch.

--- channels/file_channel.py
+++ channels/file_channel.py
@@ -22,11 +22,36 @@ OR OTHER DEALINGS IN THE SOFTWARE.
 """
 from memory_channel import ReadOnlyMemoryChannel
 from ..modifiers import Identity
+from ..utils import Printable
+
 from dateutil.parser import parse
 import os
+from semantic_version import Version
 from datetime import datetime
 import logging
 import pytz
+
+
+class FileDateTimeVersion(Printable):
+    def __init__(self, filename, split_char='_'):
+        self.long_filename, self.extension = os.path.splitext(filename)
+        
+        self.datetime, self.version = self.long_filename.split(split_char)
+        
+        self.datetime = parse(self.datetime)
+        self.version = Version(self.version[1:])
+    
+    @property
+    def is_python(self):
+        return self.extension == '.py'
+    
+    def __repr__(self):
+        return (
+            "Parsing '{self.long_filname}': \n" +
+            " Extension: {self.extension} \n" +
+            " Date time: {self.datetime} \n" +
+            "   Version: {self.version} \n"
+        ).format(**self)
 
 
 class FileChannel(ReadOnlyMemoryChannel):
@@ -40,32 +65,35 @@ class FileChannel(ReadOnlyMemoryChannel):
     timestamps are added.
     """
     path = ""
-
+    
     def get_stream_writer(self, stream_id):
         raise NotImplementedError()
-
+    
     def create_stream(self, stream_def):
         raise NotImplementedError()
-
+    
     def __init__(self, channel_id, path, up_to_timestamp=datetime.min.replace(tzinfo=pytz.utc)):
         self.path = path
         super(FileChannel, self).__init__(channel_id, up_to_timestamp)
-
+    
     def repr_stream(self, stream_id):
         s = 'externally defined by the file system, read-only stream'
         s = s + ', currently holding ' + str(len(self.streams[stream_id])) + ' files'
         return s
-
+    
     def file_filter(self, sorted_file_names):
         for file_long_name in sorted_file_names:
             if file_long_name != '__init__.py':
                 try:
-                    date_string, version = file_long_name.split('_')
-                    # Assume version is v*.py
-                    yield (parse(date_string), version[1:-3], file_long_name)
+                    file_datetime_version = FileDateTimeVersion(file_long_name)
+                    
+                    yield file_datetime_version
+                    # date_string, version = file_long_name.split('_')
+                    # # Assume version is v*.py
+                    # yield (parse(date_string), version[1:-3], file_long_name)
                 except ValueError as e:
                     logging.warn('Filename in incorrect format {0}, {1}', file_long_name, e.message)
-
+    
     def update_streams(self, up_to_timestamp):
         path = self.path
         for (long_path, dir_names, file_names) in os.walk(path):
@@ -75,9 +103,9 @@ class FileChannel(ReadOnlyMemoryChannel):
             for (file_timestamp, file_short_name, file_long_name) in self.file_filter(sorted(file_names)):
                 if file_timestamp <= up_to_timestamp:
                     self.streams[stream_id].append((file_timestamp, self.data_loader(short_path, file_long_name)))
-
+    
     def data_loader(self, short_path, file_long_name):
         raise NotImplementedError
-
+    
     def get_default_ref(self):
         return {'start': datetime.min.replace(tzinfo=pytz.utc), 'end': self.up_to_timestamp, 'modifier': Identity()}
