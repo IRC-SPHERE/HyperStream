afd2da507acf2b672f162b01c7ea90f78b0a27a9
Niall Twomey <niall.twomey@bristol.ac.uk>
2016-08-17 14:38:57 +0100

    Module loading now uses the imp module rather than the __import__ function. Tool filenames are use again major.minor.patch notation.

--- hyperstream/channels/module_channel.py
+++ hyperstream/channels/module_channel.py
@@ -28,6 +28,7 @@ from datetime import datetime
 from os.path import join
 import pytz
 import logging
+import imp
 
 
 class ModuleStreamInstance(Printable):
@@ -42,7 +43,7 @@ class ModuleChannel(FileChannel):
     respective module
     """
     versions = None
-
+    
     def update_state(self, up_to_timestamp):
         super(ModuleChannel, self).update_state(up_to_timestamp)
         versions = {}
@@ -54,26 +55,30 @@ class ModuleChannel(FileChannel):
                 name_version = name + "_" + str(version)
                 versions[name_version] = self[stream_id, datetime.min.replace(tzinfo=pytz.utc), tool_info.timestamp]
                 versions[name] = versions[name_version]
-
+    
     def file_filter(self, sorted_file_names):
         for tool_info in super(ModuleChannel, self).file_filter(sorted_file_names):
             if tool_info.is_python:
                 yield tool_info
-
+    
     def data_loader(self, short_path, tool_info):
         module_file = join(self.path, short_path, tool_info.long_filename)
         module_file_components = module_file[:-3].split('/')
         
         def module_importer():
-            logging.debug('importing ' + module_file)
-            module = __import__(module_file[:-3].replace('/', '.'))
-            for component in module_file_components[1:]:
-                module = module.__dict__[component]
+            with open(module_file, 'rb') as fp:
+                logging.debug('importing: ' + module_file)
+                module_name = '_'.join(map(lambda pp: pp.replace('.', '_'), module_file_components))
+                module = imp.load_module(
+                    module_name, fp, module_file,
+                    ('.py', 'rb', imp.PY_SOURCE)
+                )
+            
             return module
         
         # return ModuleStreamInstance(module_importer, tool_info.version)
         return tool_info.version, module_importer
-
+    
     def get_default_ref(self):
         return {'start': datetime.min.replace(tzinfo=pytz.utc), 'end': self.up_to_timestamp,
                 'modifier': Last() + IData()}
