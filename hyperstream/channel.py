59b3c5a7f2bf996b66ac64489310330b73e2dde0
Tom Diethe <tdiethe@gmail.com>
2016-08-19 04:00:34 +0100

    Sphere and database streams now loaded from mongo together. Logic to loop through workflow now working. Some other small fixes.

--- hyperstream/channel.py
+++ hyperstream/channel.py
@@ -22,7 +22,12 @@ OR OTHER DEALINGS IN THE SOFTWARE.
 """
 from utils import Printable, utcnow
 from channels import ToolChannel, SphereChannel, MemoryChannel, DatabaseChannel
-from errors import StreamNotFoundError
+from errors import StreamNotFoundError, StreamAlreadyExistsError
+from models import StreamDefinitionModel
+from stream import StreamId, StreamReference
+from modifiers import Identity
+
+import logging
 
 
 class ChannelCollection(Printable):
@@ -32,13 +37,46 @@ class ChannelCollection(Printable):
         self.memory_channel = MemoryChannel("memory")
         self.database_channel = DatabaseChannel("mongo")
 
-    def __getitem__(self, item):
-        if item in self.memory_channel.streams:
-            return self.memory_channel.streams[item]
-        if item in self.database_channel.streams:
-            return self.database_channel.streams[item]
-        # if item in self.tool_channel.streams:
-        #     return self.tool_channel.streams[item]
-        if item in self.sphere_channel.streams:
-            return self.sphere_channel.streams[item]
-        raise StreamNotFoundError("{} not found in channels".format(item))
+        # TODO: find a more elegant solution for this
+        self.channels = {
+            'tools': self.tool_channel,
+            'sphere': self.sphere_channel,
+            'memory': self.memory_channel,
+            'database': self.database_channel
+        }
+
+        self.update_channels()
+        for channel in self.channels:
+            logging.debug(channel)
+            logging.debug(self.channels[channel])
+            logging.debug("")
+
+    def get_stream(self, stream_id):
+        if stream_id in self.memory_channel:
+            return self.memory_channel[stream_id]
+        if stream_id in self.database_channel:
+            return self.database_channel[stream_id]
+        if stream_id in self.tool_channel:
+            return self.tool_channel[stream_id]
+        if stream_id in self.sphere_channel:
+            return self.sphere_channel[stream_id]
+        raise StreamNotFoundError("{} not found in channels".format(stream_id))
+
+    def update_channels(self):
+        for s in StreamDefinitionModel.objects():
+            stream_id = StreamId(name=s.stream_id.name, meta_data=s.stream_id.meta_data)
+            channel = self.channels[s.channel]
+
+            # TODO: Do we need a TimeInterval defined here?
+            # TODO: Do we need anything other than Identity() for the modifier here?
+
+            if stream_id in channel.streams:
+                raise StreamAlreadyExistsError(stream_id)
+
+            channel.streams[stream_id] = StreamReference(
+                channel_id=s.channel,
+                stream_id=stream_id,
+                time_interval=None,
+                modifier=Identity(),
+                get_results_func=channel.get_results
+            )
