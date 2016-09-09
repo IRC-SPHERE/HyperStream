9d4f3123c7062f52bc72904848e3dd2a3582b7c8
Niall Twomey <niall.twomey@bristol.ac.uk>
2016-09-10 00:33:51 +0100

    Implemented node/factor based workflow in main_niall.py Some minor changes (e.g. in stream.py in get_aligning_node)

--- main_niall.py
+++ main_niall.py
@@ -51,25 +51,119 @@ if __name__ == '__main__':
     
     # A couple of parameters
     t1 = datetime(2016, 4, 28, 20, 0, 0, 0, UTC)
-    interval = TimeInterval(t1, t1 + timedelta(minutes=2))
+    interval = TimeInterval(t1, t1 + timedelta(minutes=60))
     relative_interval = RelativeTimeInterval(-30, 0)
     
     minute = timedelta(minutes=1)
     second = timedelta(seconds=1)
     
+    w = Workflow(
+        channels=channels,
+        plates=plates,
+        workflow_id="nt_test",
+        name="test",
+        owner="nt",
+        description="test")
+    
+    # Create clock stream
+    n_clock = w.create_node(
+        stream_name='every_10_seconds',
+        channel=M,
+        plate_ids=None)
+    f_clock = w.create_factor(
+        tool_name='clock',
+        tool_parameters={
+            'first': MIN_DATE,
+            'stride': 10.0
+        },
+        source_nodes=None,
+        sink_node=n_clock,
+        alignment_node=None)
+    
+    # Create the env data stream
+    n_environmental_data = w.create_node(
+        stream_name='environmental',
+        channel=M,
+        plate_ids=['H1'])
+    f_environmental_data = w.create_factor(
+        tool_name='sphere',
+        tool_parameters={
+            'modality': 'environmental',
+            'filters': {
+                'uid': {
+                    '$regex': r'S\d+_K'
+                }
+            }
+        },
+        source_nodes=None,
+        sink_node=n_environmental_data,
+        alignment_node=None)
+    
+    # Apply a relative window to this stream
+    n_relative_env_data = w.create_node(
+        stream_name='relative_environmental_data',
+        channel=M,
+        plate_ids=['H1'])
+    f_relative_env_data = w.create_factor(
+        tool_name='relative_window',
+        tool_parameters={
+            'relative_start': -30,
+            'relative_end': 0,
+            'values_only': False
+        },
+        source_nodes=[
+            n_environmental_data
+        ],
+        sink_node=n_relative_env_data,
+        alignment_node=n_clock)
+    
+    # Do aggregation over the the reative windows data
+    n_relative_mean = w.create_node(
+        stream_name='relative_mean_env_data',
+        channel=M,
+        plate_ids=['H1'])
+    f_relative_mean = w.create_factor(
+        tool_name='relative_apply',
+        tool_parameters={
+            'func': online_average
+        },
+        source_nodes=[
+            n_relative_env_data
+        ],
+        sink_node=n_relative_mean,
+        alignment_node=n_clock)
+    
+    w.execute(interval)
+    
+    rows = []
+    for kk, node in n_relative_mean.streams.iteritems():
+        for time, values in node.window(interval).iteritems():
+            rows.append(values)
+            rows[-1]['datetime'] = time
+    
+    import pandas as pd
+    pd.options.display.width = 200
+    df = pd.DataFrame(rows)
+    df.set_index('datetime', inplace=True)
+    
+    print df
+    
+    exit()
+    
     sid_every_second = StreamId('every30s')
     environmental = StreamId('environmental_niall', meta_data={'house': '1'})
     
-    ti = TimeInterval(t1, t1 + timedelta(minutes=2))
+    ti = TimeInterval(t1, t1 + timedelta(minutes=1))
     rel = RelativeTimeInterval(-30 * second, 0)
     
     # Create the synch tool
     M.create_stream(stream_id=sid_every_second)
     t_clock = channels.get_tool('clock', dict(first=MIN_DATE, stride=1 * second))
-
+    
     # Get data from database
     # D.create_stream(environmental)
-    t_environmental = channels.get_tool('sphere', dict(modality='environmental', filters={'uid': {'$regex': r'S\d+_K'}}))
+    t_environmental = channels.get_tool('sphere',
+                                        dict(modality='environmental', filters={'uid': {'$regex': r'S\d+_K'}}))
     
     # Apply relative window
     window_id = StreamId('sid_window_environmental_niall')
@@ -109,7 +203,7 @@ if __name__ == '__main__':
         print time
         for value in values:
             print '\t', value
-        print
+        break
     
     exit()
     
