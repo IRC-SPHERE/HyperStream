4576ff797126e1ba32f489f972da8464e6a0051b
Meelis Kull <meelis.kull@gmail.com>
2016-10-19 14:44:26 +0100

    multiresident experiments from Sion have been run through the hyperstreamify.py script resulting in location_annotations.csv; now ready to be inserted into hyperstream as the next step

--- data/2016_10_17_multiresident_annotations_from_sion_anonymised/hyperstreamify.py
+++ data/2016_10_17_multiresident_annotations_from_sion_anonymised/hyperstreamify.py
@@ -1,7 +1,7 @@
 
 import pandas as pd
 
-exper = pd.read_csv('experiments_anonymised.txt')
+exper = pd.read_csv('occurrence_times_manually.txt')
 print(exper)
 
 max_cols = 20
@@ -9,9 +9,8 @@ data_thu = pd.read_csv('wp5Data1ThursBB3.csv',header=None,names=range(max_cols))
 data_thu.columns = ['camera_id','dt','bt']+range(3,max_cols)
 data_fri = pd.read_csv('wp5Data1FriBB3.csv',header=None,names=range(max_cols))
 data_fri.columns = ['camera_id','dt','bt']+range(3,max_cols)
-#data_thu = data_thu.dropna(axis=1,how='all')
-#d = data_thu[:,2:max_cols]
-
+data = data_thu.append(data_fri)
+data.index = range(data.shape[0])
 
 # 3,1467900022735,11765191481,56794274,56794275
 
@@ -20,22 +19,60 @@ data_fri.columns = ['camera_id','dt','bt']+range(3,max_cols)
 # 11765191481 is bt = 11765191481
 # 56794274,56794275 are bounding box ids
 
-coords_thu = pd.read_csv('wp5bbCoordsFriBB3.csv',header=None)
+coords_thu = pd.read_csv('wp5bbCoordsThursBB3.csv',header=None)
 coords_fri = pd.read_csv('wp5bbCoordsFriBB3.csv',header=None)
 coords_thu.columns = ['bb_id','person_id','bb2d_0','bb2d_1','bb2d_2','bb2d_3','bb3d_0','bb3d_1','bb3d_2','bb3d_3','bb3d_4','bb3d_5']
 coords_fri.columns = ['bb_id','person_id','bb2d_0','bb2d_1','bb2d_2','bb2d_3','bb3d_0','bb3d_1','bb3d_2','bb3d_3','bb3d_4','bb3d_5']
+coords = coords_thu.append(coords_fri)
+coords.index = range(coords.shape[0])
 
 # 56794274, -1, 76, 8, 347, 403, -1107, 1053, 2588, 150, -911, 3188
 # 56794275, 1, 300, 56, 383, 371, -86, 796, 2469, 339, -704, 3069
 bb2person = {}
-for i in range(10000): #range(coords_thu.shape[0]):
-    bb2person[coords_thu.bb_id[i]] = coords_thu.person_id[i]
+print('working through coords')
+for (i,row) in coords.iterrows():
+    if (i % 10000)==0:
+        print(i)
+    if row.person_id>=1:
+        bb2person[row.bb_id] = row.person_id
 
-#person_occurrences = {timestamp:location}
-# create a dataframe with dt,camera_id,person_id
-#
-# add to it the experiment_id and wearable_id
+res = pd.DataFrame()
+rows = []
+out_of_experiment = {}
+print('working through data')
+for i in range(data.shape[0]):
+    if (i % 10000)==0:
+        print(i)
+    bb_ids = data.loc[i][3:max_cols]
+    for bb_id in bb_ids:
+        if bb2person.has_key(bb_id) and bb2person[bb_id]>=1:
+            exp_id = -1
+            wearable_id = 'NA'
+            for j in range(exper.shape[0]):
+                if (exper.person_id[j]==bb2person[bb_id]) and (exper.first_occurrence[j]<=data.dt[i]) and (exper.last_occurrence[j]>=data.dt[i]):
+                    exp_id = exper.experiment[j]
+                    wearable_id = exper.wearable[j]
+            if (exp_id==0): #or (bb2person[bb_id]==5):
+                out_of_experiment[bb2person[bb_id]] = data.dt[i] # on Friday person number 5 was hanging around outside of the experiment times
+            if exp_id>0:
+                rows.append({'dt':data.dt[i],'camera_id':data.camera_id[i],'person_id':bb2person[bb_id],'exper_id':exp_id,'wearable_id':wearable_id})
+res = pd.DataFrame(rows)
+print(out_of_experiment)
 
-a=1
+res.to_csv('location_annotations.csv', sep=',', index=False)
+
+
+# in the range [1467985042332,1467985361449] person 5 was moving around
+# in one experiment person 1 forgot to put on the wearable
+
+
+#w = res.loc[res['person_id'] == 24]
+#w = res.loc[(res['person_id'] == 9)] # and (res['dt']>0)]
+#print(w.iloc[-1])
+
+#print(res.loc[res['person_id']==9].iloc[0])
+
+#w = res.loc[(res['person_id'] == 5)] # and (res['dt']>0)]
+#print(w.iloc[-1])
 
 
