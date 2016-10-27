60f482ac54a17f08b647c9bb88a33b7d417315ec
Tom Diethe <tdiethe@gmail.com>
2016-10-27 14:18:58 +0100

    Removed SubArray for now. First implementation of IndexOf

--- hyperstream/tools/index_of/2016-10-26_v0.1.0.py
+++ hyperstream/tools/index_of/2016-10-26_v0.1.0.py
@@ -18,9 +18,7 @@
 #  OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
 #  OR OTHER DEALINGS IN THE SOFTWARE.
 
-from hyperstream.stream import StreamInstance
 from hyperstream.tool import AggregateTool
-from hyperstream.utils import MIN_DATE
 
 
 class IndexOf(AggregateTool):
@@ -33,29 +31,9 @@ class IndexOf(AggregateTool):
 
     def _execute(self, sources, alignment_stream, interval):
 
-        # Put all of the data in a dict of sorted lists (inefficient!)
-        data = dict((source.stream_id,
-                     sorted(source.window(interval, force_calculation=True), key=lambda x: x.timestamp))
-                    for source in sources)
+        source = next(s for s in sources if s.stream_id.meta_data == self.index)
+        if not source:
+            raise IndexError("Index {} not found in sources".format(self.index))
 
-        # Create a set of all of the timestamps available (also inefficient!)
-        timestamps = sorted(set(item.timestamp for d in data.values() for item in d))
-
-        # maintain dict of indices where the timestamps appear
-        last_timestamps = dict((stream_id, MIN_DATE) for stream_id in data)
-
-        # Now loop through the timestamps, and aggregate over the aggregation plate
-        for ts in timestamps:
-            values = []
-            for stream_id in data:
-                for item in data[stream_id]:
-                    if item.timestamp < last_timestamps[stream_id]:
-                        continue
-                    if item.timestamp < ts:
-                        continue
-                    if item.timestamp == ts:
-                        values.append(item.value)
-                    last_timestamps[stream_id] = item.timestamp
-                    break
-            yield StreamInstance(ts, self.func(values))
+        return source.window(interval)
 
