35950056e578ee9e9e147f42413aa77c3c08958f
Niall Twomey <niall.twomey@bristol.ac.uk>
2016-11-08 13:52:42 +0000

    Localisation modelling performed in tools.
    Notes on aspects that may change
    * Parameters for prediction are selected as the last element of the stream over all time.
    * Need to ensure that predictions

--- plugins/sphere/tools/localisation_model_learn/__init__.py
+++ plugins/sphere/tools/localisation_model_learn/__init__.py
@@ -18,33 +18,3 @@
 # DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
 # OR OTHER DEALINGS IN THE SOFTWARE.
-
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.model_selection import PredefinedSplit
-import pandas as pd
-
-data = [
-]
-
-feature_columns = []
-label_column = ''
-fold_column = ''
-
-# Generate a data frame
-df = pd.DataFrame([datum['x'].merge(datum['y']) for datum in data])
-
-# Fill empty values, and drop any rows for which the label/fold index is NaN
-df[feature_columns].fillna(-120)
-df.isnull().any(axis=1)
-df.dropna(subset=[label_column, fold_column], inplace=True)
-
-# Use pre-defined folds for model selection
-folds = PredefinedSplit(df[fold_column])
-for train_inds, test_inds in folds.split():
-    train_x = df[feature_columns].iloc[train_inds]
-    test_x = df[feature_columns].iloc[test_inds]
-    train_y = df[label_column].iloc[train_inds]
-    test_y = df[label_column].iloc[test_inds]
-    
-    clf = LinearDiscriminantAnalysis()
-    clf.fit(train_x.data, train_y.data)
