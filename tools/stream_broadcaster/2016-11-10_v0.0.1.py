08b0c76d8b9dc350144cb33800fc8985d2f36194
Niall Twomey <niall.twomey@bristol.ac.uk>
2016-11-10 15:52:46 +0000

    Updated deployment script.

--- tools/stream_broadcaster/2016-11-10_v0.0.1.py
+++ tools/stream_broadcaster/2016-11-10_v0.0.1.py
@@ -18,3 +18,24 @@
 # DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
 # OR OTHER DEALINGS IN THE SOFTWARE.
+
+from hyperstream.tool import MultiOutputTool
+from hyperstream.stream import StreamMetaInstance
+from hyperstream.time_interval import TimeInterval
+from hyperstream.utils import MIN_DATE, MAX_DATE
+
+
+class StreamBroadcaster(MultiOutputTool):
+    def __init__(self, mapping):
+        super(StreamBroadcaster, self).__init__(mapping=mapping)
+        self.mapping = mapping
+    
+    def _execute(self, source, interval, output_plate):
+        # TODO: This factor is currently used to pull out the parameters of a localisation model, and as such does \
+        #   use the time interval, but only pulls the last instance in the stream. Will need to change this in \
+        #   future instances
+        param_doc = source.window(TimeInterval(MIN_DATE, MAX_DATE), force_calculation=True).last()
+        
+        for kk, vv in self.mapping.iteritems():
+            yield StreamMetaInstance(
+                (param_doc.timestamp, param_doc.value), (output_plate.meta_data_id, str(vv)))
