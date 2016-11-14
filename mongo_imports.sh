f40896c7314ac6a2a3a77b0dda95dac34fe178f4
Tom Diethe <tdiethe@gmail.com>
2016-11-14 23:19:26 +0000

    added --drop

--- mongo_imports.sh
+++ mongo_imports.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-mongoimport -h=localhost:27017 -d=hyperstream -c=meta_data --file=mongo_dumps/meta_data.txt
-mongoimport -h=localhost:27017 -d=hyperstream -c=plate_definitions --file=mongo_dumps/plate_definitions.txt
-mongoimport -h=localhost:27017 -d=hyperstream -c=workflow_definitions --file=mongo_dumps/workflow_definitions.txt
-mongoimport -h=localhost:27017 -d=hyperstream -c=workflow_status --file=mongo_dumps/workflow_status.txt
+mongoimport -h=localhost:27017 -d=hyperstream --drop -c=meta_data --file=mongo_dumps/meta_data.txt
+mongoimport -h=localhost:27017 -d=hyperstream --drop -c=plate_definitions --file=mongo_dumps/plate_definitions.txt
+mongoimport -h=localhost:27017 -d=hyperstream --drop -c=workflow_definitions --file=mongo_dumps/workflow_definitions.txt
+mongoimport -h=localhost:27017 -d=hyperstream --drop -c=workflow_status --file=mongo_dumps/workflow_status.txt
