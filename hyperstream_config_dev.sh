5ea8b9f10247970f90be44e2e7c2b05b654acd5d
Tom Diethe <tdiethe@gmail.com>
2016-11-23 16:50:49 +0000

    Some extra configs

---
+++ hyperstream_config_dev.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+rm hyperstream_config.json
+ln -s hyperstream_config_dev.json hyperstream_config.json
---
+++ hyperstream_config_shg.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+rm hyperstream_config.json
+ln -s hyperstream_config_shg.json hyperstream_config.json
