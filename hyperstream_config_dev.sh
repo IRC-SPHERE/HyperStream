63c9714866da469ca94c77a9872fabf6826ae813
Tom Diethe <tdiethe@gmail.com>
2016-11-29 12:21:22 +0000

    reverting bad commits

--- hyperstream_config_dev.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-rm hyperstream_config.json
-ln -s hyperstream_config_dev.json hyperstream_config.json
--- mongo_exports.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-mongoexport -h=localhost:27018 -u=dbuser -p=rYYr7QEeD5 --authenticationDatabase=admin --authenticationMechanism=MONGODB-CR -d=hyperstream -c=meta_data --out=mongo_dumps/meta_data.txt
-mongoexport -h=localhost:27018 -u=dbuser -p=rYYr7QEeD5 --authenticationDatabase=admin --authenticationMechanism=MONGODB-CR -d=hyperstream -c=plate_definitions --out=mongo_dumps/plate_definitions.txt
-mongoexport -h=localhost:27018 -u=dbuser -p=rYYr7QEeD5 --authenticationDatabase=admin --authenticationMechanism=MONGODB-CR -d=hyperstream -c=workflow_definitions --out=mongo_dumps/workflow_definitions.txt
-mongoexport -h=localhost:27018 -u=dbuser -p=rYYr7QEeD5 --authenticationDatabase=admin --authenticationMechanism=MONGODB-CR -d=hyperstream -c=workflow_status --out=mongo_dumps/workflow_status.txt
