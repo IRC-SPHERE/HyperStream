42de9734bb6255df584437c81fabaf5f231b6277
Meelis Kull <meelis.kull@gmail.com>
2016-12-15 07:28:48 +0000

    modified and new tools: dict_argmax, dict_mean, histogram_from_list

---
+++ hyperstream/tools/dict_mean/2016-12-15_v0.0.1.py
@@ -0,0 +1,41 @@
+# The MIT License (MIT) # Copyright (c) 2014-2017 University of Bristol
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in all
+#  copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+#  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+#  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+#  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+#  OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+#  OR OTHER DEALINGS IN THE SOFTWARE.
+
+from hyperstream.stream import StreamInstance
+from hyperstream.tool import Tool, check_input_stream_count
+
+
+class DictArgmax(Tool):
+    """
+    Simple tool that picks out a component of the data dict
+    """
+    def __init__(self):
+        super(DictArgmax, self).__init__()
+
+    @check_input_stream_count(1)
+    def _execute(self, sources, alignment_stream, interval):
+        for time, data in sources[0].window(interval, force_calculation=True):
+            max_value = None
+            argmax = None
+            for key in data.keys():
+                if max_value is None or data[key]>max_value:
+                    max_value = data[key]
+                    argmax = key
+            yield StreamInstance(time, argmax)
