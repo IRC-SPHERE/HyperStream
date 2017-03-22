d023109cac627bc86ba61c430d23cf63f0e57999
Meelis Kull <meelis.kull@gmail.com>
2017-03-22 16:54:44 +0000

    sliding_sink tool improved to match calculated times with windows

---
+++ hyperstream/tools/sliding_sink/2017-03-16_v0.0.2.py
@@ -0,0 +1,45 @@
+"""
+The MIT License (MIT)
+Copyright (c) 2014-2017 University of Bristol
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+from hyperstream import TimeInterval
+from hyperstream.tool import Tool, check_input_stream_count
+
+class SlidingSink(Tool):
+    def __init__(self):
+        super(SlidingSink, self).__init__()
+
+    # noinspection PyCompatibility
+    @check_input_stream_count(2)
+    def _execute(self, sources, alignment_stream, interval):
+        sliding_window = sources[0].window(interval, force_calculation=True)
+        first_window = sliding_window.first()
+        if first_window is None:
+            return
+        try:
+            last_window = list(sliding_window)[-1]
+        except:
+            last_window = first_window
+        sources[1].window(TimeInterval(first_window.start, last_window.end), force_calculation=True).first()
+        return
+        yield # required to make this function into a generator
+
