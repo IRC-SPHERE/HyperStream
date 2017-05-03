114698abf85890e66c6d7671dcaccc9727f2df4c
Tom Diethe <tdiethe@gmail.com>
2017-05-03 22:03:54 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from distutils.util import convert_path
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+main_ns = {}
+ver_path = convert_path('hyperstream/version.py')
+with open(ver_path) as ver_file:
+    exec(ver_file.read(), main_ns)
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+   name='hyperstream',
+   version=main_ns['__version__'],
+   description='Hyperstream is a large-scale, flexible and robust software package for processing streaming data',
+   license="MIT",
+   long_description=long_description,
+   author='Tom Diethe; Meelis Kull; Niall Twomey; Kacper Sokol; Hao Song; Emma Tonkin; Peter Flach',
+   author_email='hyperstreamhq@googlegroups.com',
+   url="https://irc-sphere.github.io/HyperStream/",
+   packages=['hyperstream'],  #same as name
+   install_requires=required, #external packages as dependencies
+   scripts=[]
+)
