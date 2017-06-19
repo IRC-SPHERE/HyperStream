61949f8cb925dfccd4eee636c61f6c591e3f99bb
Miquel Perelló Nieto <perello.nieto@gmail.com>
2017-06-19 16:55:53 +0100

    File channels more robust. - Now the file channels look only for Python code. - The __init__.py files are still filtered - If the date from the filename is incorrect raises an ValueError that is captured and the following file in list is tried.

--- hyperstream/channels/file_channel.py
+++ hyperstream/channels/file_channel.py
@@ -37,9 +37,12 @@ class FileDateTimeVersion(Printable):
         self.long_filename = filename
         self.filename_no_extension, self.extension = os.path.splitext(filename)
         self.timestamp, self.version = self.filename_no_extension.split(split_char, 1)
-        self.timestamp = ciso8601.parse_datetime(self.timestamp).replace(tzinfo=UTC)
+        self.timestamp = ciso8601.parse_datetime(self.timestamp)
+        if not self.timestamp:
+            raise ValueError("Invalid timestamp {}".format(self.timestamp))
+        self.timestamp = self.timestamp.replace(tzinfo=UTC)
         self.version = Version(self.version[1:])
-    
+
     @property
     def is_python(self):
         return self.extension == '.py'
@@ -56,57 +59,57 @@ class FileChannel(ReadOnlyMemoryChannel):
     timestamps are added.
     """
     path = ""
-    
+
     def __init__(self, channel_id, path, up_to_timestamp=MIN_DATE):
         self.path = path
         super(FileChannel, self).__init__(channel_id=channel_id, up_to_timestamp=up_to_timestamp)
 
     def file_filter(self, sorted_file_names):
         for file_long_name in sorted_file_names:
-            if file_long_name[:11] != '__init__.py' and file_long_name[-3:] != 'pyc':
+            if file_long_name[:11] != '__init__.py' and file_long_name[-3:] == '.py':
                 try:
                     tool_info = FileDateTimeVersion(file_long_name)
-                    
+
                     yield tool_info
-                
+
                 except ValueError as e:
                     logging.warn('Filename in incorrect format {0}, {1}'.format(file_long_name, e.message))
-    
+
     def update_streams(self, up_to_timestamp):
         path = self.path
         for (long_path, dir_names, file_names) in os.walk(path):
             file_names = filter(lambda ff: ff != '__init__.py', file_names)
             if len(file_names) == 0:
                 continue
-            
+
             name = long_path[len(path) + 1:]
             if not name:
                 # Empty folder
                 continue
-            
+
             stream_id = StreamId(name=name)
             stream = Stream(channel=self, stream_id=stream_id, calculated_intervals=None, sandbox=None)
             self.streams[stream_id] = stream
 
     def data_loader(self, short_path, file_info):
         raise NotImplementedError
-    
+
     def get_results(self, stream, time_interval):
         # TODO: Make this behave like the other channels
         # if relative_time_interval.end > self.up_to_timestamp:
         #     raise ValueError(
         #         'The stream is not available after ' + str(self.up_to_timestamp) + ' and cannot be calculated')
-        
+
         result = []
         module_path = os.path.join(self.path, stream.stream_id.name)
-        
+
         for file_info in self.file_filter(sorted(os.listdir(module_path))):
             if file_info.timestamp in time_interval and file_info.timestamp <= self.up_to_timestamp:
                 result.append(StreamInstance(
                     timestamp=file_info.timestamp,
                     value=self.data_loader(stream.stream_id.name, file_info)
                 ))
-        
+
         result.sort(key=lambda x: x.timestamp)
         return result
 
