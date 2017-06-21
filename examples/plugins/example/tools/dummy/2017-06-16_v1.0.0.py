d5d3d202d304a5d1893e1bf98d5848d032125f77
Miquel Perelló Nieto <perello.nieto@gmail.com>
2017-06-21 19:23:14 +0100

    Created selfcontained tutorial plugins and configuration

---
+++ examples/plugins/example/tools/dummy/2017-06-16_v1.0.0.py
@@ -0,0 +1,37 @@
+# The MIT License (MIT)
+# Copyright (c) 2014-2017 University of Bristol
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+# OR OTHER DEALINGS IN THE SOFTWARE.
+
+from hyperstream import Tool
+from hyperstream.utils import check_input_stream_count
+
+
+class Dummy(Tool):
+    """
+    Dummy tool that just returns the data provided to it
+    """
+    def __init__(self):
+        super(Dummy, self).__init__()
+
+    @check_input_stream_count(1)
+    def _execute(self, sources, alignment_stream, interval):
+        data = sources[0].window(interval, force_calculation=True)
+        for d in data:
+            yield d
