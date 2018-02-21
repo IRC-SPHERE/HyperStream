321e7f2b0020eef67fe47ea5cec39e9260e9abec
SPHERE deploy <sphere.deploy@gmail.com>
2018-02-21 17:18:09 +0000

    Fixed problem with indentation

--- hyperstream/utils/hyperstream_logger.py
+++ hyperstream/utils/hyperstream_logger.py
@@ -52,7 +52,7 @@ class HyperStreamLogger(Printable):
         :param close_existing: Whether to close existing loggers
         """
         # noinspection SpellCheckingInspection
-                if default_loglevel == logging.DEBUG:
+        if default_loglevel == logging.DEBUG:
             log_formatter = logging.Formatter(u"%(asctime)s [%(levelname)-5.5s] %(filename)s:%(lineno)d %(message)s")
         else:
             log_formatter = logging.Formatter(u"%(asctime)s [%(levelname)-5.5s]  %(message)s")
